6a231e14e679a078195d1781b060a36bede76bdb
Ashwin Menghar <ashwinmenghar123@gmail.com>
2024-06-22 10:30:17 +0530

    minor update

--- src/components/CollegeTable.jsx
+++ src/components/CollegeTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../CollegeTable.css";
 import colleges from "../constant/colleges.json";
 
